088f683f03f41f4267f9d7aec5ebeaacd832f395
theunholytim <helena@canyourwindowsdothat>
2023-06-14 16:39:58 -0300

    very important disclaimer.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# NOTE THAT THIS WAS BUILT TO WORK ONLY ON _MY MACHINE_
+# this is built thinking about x86_64 linux architecture, made for educational purposes only
+
 import mirror_parser as mp;
 
 if __name__ == "__main__":
